afe7eec0666c019cf4f2f24ccc78c916e1988a62
pakula <pakula@newrelic.com>
2023-07-20 17:10:18 +0530

    commit the publish

---
+++ my-app/build/precache-manifest.5856f2585612b9c96760b13bd68e124a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d4d935061ca375dba37",
+    "url": "/my-app/static/css/main.c9566324.chunk.css"
+  },
+  {
+    "revision": "8d4d935061ca375dba37",
+    "url": "/my-app/static/js/main.7a1f5601.chunk.js"
+  },
+  {
+    "revision": "a9e9488bf1c79412ffe2",
+    "url": "/my-app/static/js/runtime~main.ad3febe6.js"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/my-app/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/my-app/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/my-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "7e41d7c466fcce9628f232da8eb99a6b",
+    "url": "/my-app/index.html"
+  }
+];
