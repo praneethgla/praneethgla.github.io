35d5f9e3ecb5150b56937d947b3243740308201d
pakula <pakula@newrelic.com>
2023-07-26 02:23:54 +0530

    new pakages router added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Praneeth test site today</p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      <h2>Home View</h2>
+      <p>this is home view by praneeth</p>
     </div>
   );
+}
+function About() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>About View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+function GoogleHtml() {
+  return (<>
+  
+    google-site-verification: google7c471d28facd7f9e.html</>
+  );
+}
+function App() {
+  return(
+    <Router>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about.html" element={<About />} />
+    <Route path="/google7c471d28facd7f9e.html" element={<GoogleHtml />} ></Route>
+  </Routes>
+  </Router>
+  )
 }
 
 export default App;
