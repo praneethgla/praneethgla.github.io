ae6c5f531343a1a71adc97a4c4f1c7f481d4e3ed
pakula <pakula@newrelic.com>
2023-07-26 01:22:11 +0530

    Updates

---
+++ precache-manifest.a2bd633cc4a1b43c50579b6e00c03f02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a321b5a962c8b4d4090e",
+    "url": "/static/css/main.cdce58b5.chunk.css"
+  },
+  {
+    "revision": "a321b5a962c8b4d4090e",
+    "url": "/static/js/main.a321b5a9.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "45fd8c917d9687a991cf",
+    "url": "/static/js/runtime~main.45fd8c91.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "a8e1266dba22197bbf5867f72d86d601",
+    "url": "/index.html"
+  }
+];
