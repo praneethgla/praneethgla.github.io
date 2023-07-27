f3a810ce10552bd3e423baa150b6580b7178c4af
pakula <pakula@newrelic.com>
2023-07-28 00:58:06 +0530

    changes adding route

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 
-function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h2>Home View</h2>
-      <p>this is home view by praneeth</p>
-    </div>
-  );
-}
+import Home from "./components/home/home";
+
 function About() {
   return (
     <div style={{ padding: 20 }}>
@@ -20,21 +14,41 @@ function About() {
   );
 }
 function GoogleHtml() {
-  return (<>
-  
-    google-site-verification: google7c471d28facd7f9e.html</>
+  return <>google-site-verification: google7c471d28facd7f9e.html</>;
+}
+
+function CustomHeader() {
+  return (
+    <>
+      <div className="p-4 row">
+        <div className="col-3">
+          <Link to="/">Home</Link>
+        </div>
+        <div className="col-3">
+          <Link to="/about.html">About</Link>
+        </div>
+        <div className="col-3"></div>
+      </div>
+    </>
   );
 }
 function App() {
-  return(
-    <Router>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/about.html" element={<About />} />
-    <Route path="/google7c471d28facd7f9e.html" element={<GoogleHtml />} ></Route>
-  </Routes>
-  </Router>
-  )
+  return (
+    <>
+    
+      <Router>
+      <CustomHeader />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about.html" element={<About />} />
+          <Route
+            path="/google7c471d28facd7f9e.html"
+            element={<GoogleHtml />}
+          ></Route>
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
---
+++ my-app/src/components/home/home.tsx
@@ -0,0 +1,12 @@
+
+
+const Home = ()=> {
+    return (
+      <div style={{ padding: 20 }}>
+        <h2>Home View</h2>
+        <p>this is home view by praneeth</p>
+      </div>
+    );
+  };
+
+  export default Home;
